6a196bb1a889af6236c8fb0a80e4901642cc60e0
Jan Pingel <jpingel@bitpiston.com>
2013-05-27 01:30:32 -0600

    Changed table and column names for migrating from Drupal 6

--- Drupal2Wordpress.php
+++ Drupal2Wordpress.php
@@ -6,14 +6,14 @@
 	$DB_HOSTNAME	= 'localhost';
 	
 	//Wordpress Database Name, Username and Password
-	$DB_WP_USERNAME	= 'root';
-	$DB_WP_PASSWORD	= 'root';
-	$DB_WORDPRESS	= 'robin_wordpress';
+	$DB_WP_USERNAME	= 'user';
+	$DB_WP_PASSWORD	= 'pass';
+	$DB_WORDPRESS	= 'database';
 
 	//Drupal Database Name, Username and Password
-	$DB_DP_USERNAME	= 'root';
-	$DB_DP_PASSWORD	= 'root';
-	$DB_DRUPAL		= 'robin_drupal';
+	$DB_DP_USERNAME	= 'user';
+	$DB_DP_PASSWORD	= 'pass';
+	$DB_DRUPAL		= 'database';
 
 	//Table Prefix
 	$DB_WORDPRESS_PREFIX = 'wp_';
@@ -51,14 +51,14 @@
 	message('Wordpress Table Truncated');
 	
 	//Get all drupal Tags and add it into worpdress terms table
-	$drupal_tags = $dc->results("SELECT DISTINCT d.tid, d.name, REPLACE(LOWER(d.name), ' ', '_') AS slug FROM ".$DB_DRUPAL_PREFIX."taxonomy_term_data d INNER JOIN ".$DB_DRUPAL_PREFIX."taxonomy_term_hierarchy h ON (d.tid = h.tid) ORDER BY d.tid ASC");
+	$drupal_tags = $dc->results("SELECT DISTINCT d.tid, d.name, REPLACE(LOWER(d.name), ' ', '-') AS slug FROM ".$DB_DRUPAL_PREFIX."term_data d INNER JOIN ".$DB_DRUPAL_PREFIX."term_hierarchy h ON (d.tid = h.tid) ORDER BY d.tid ASC");
 	foreach($drupal_tags as $dt)
 	{
 		$wc->query("REPLACE INTO ".$DB_WORDPRESS_PREFIX."terms (term_id, name, slug) VALUES ('%s','%s','%s')", $dt['tid'], $dt['name'], $dt['slug']);
 	}
 
 	//Update worpdress term_taxonomy table
-	$drupal_taxonomy = $dc->results("SELECT DISTINCT d.tid AS term_id, 'post_tag' AS post_tag, d.description AS description, h.parent AS parent FROM ".$DB_DRUPAL_PREFIX."taxonomy_term_data d INNER JOIN ".$DB_DRUPAL_PREFIX."taxonomy_term_hierarchy h ON (d.tid = h.tid) ORDER BY 'term_id' ASC");
+	$drupal_taxonomy = $dc->results("SELECT DISTINCT d.tid AS term_id, 'post_tag' AS post_tag, d.description AS description, h.parent AS parent FROM ".$DB_DRUPAL_PREFIX."term_data d INNER JOIN ".$DB_DRUPAL_PREFIX."term_hierarchy h ON (d.tid = h.tid) ORDER BY 'term_id' ASC");
 	foreach($drupal_taxonomy as $dt)
 	{
 		$wc->query("INSERT INTO ".$DB_WORDPRESS_PREFIX."term_taxonomy (term_id, taxonomy, description, parent) VALUES ('%s','%s','%s','%s')", $dt['term_id'], $dt['post_tag'], $dt['description'], $dt['parent']);
@@ -67,7 +67,7 @@
 	message('Tags Updated');
 
 	//Get all post from Drupal and add it into wordpress posts table
-	$drupal_posts = $dc->results("SELECT DISTINCT n.nid AS id, n.uid AS post_author, FROM_UNIXTIME(n.created) AS post_date, r.body_value AS post_content, n.title AS post_title, r.body_summary AS post_excerpt, n.type AS post_type,  IF(n.status = 1, 'publish', 'private') AS post_status FROM ".$DB_DRUPAL_PREFIX."node n, ".$DB_DRUPAL_PREFIX."field_data_body r WHERE (n.vid = r.entity_id)");
+	$drupal_posts = $dc->results("SELECT DISTINCT n.nid AS id, n.uid AS post_author, FROM_UNIXTIME(n.created) AS post_date, r.body AS post_content, n.title AS post_title, r.teaser AS post_excerpt, n.type AS post_type,  IF(n.status = 1, 'publish', 'private') AS post_status FROM ".$DB_DRUPAL_PREFIX."node n, ".$DB_DRUPAL_PREFIX."node_revisions r WHERE (n.vid = r.vid)");
 	foreach($drupal_posts as $dp)
 	{
 		$wc->query("INSERT INTO ".$DB_WORDPRESS_PREFIX."posts (id, post_author, post_date, post_content, post_title, post_excerpt, post_type, post_status) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s')", $dp['id'], $dp['post_author'], $dp['post_date'], $dp['post_content'], $dp['post_title'], $dp['post_excerpt'], $dp['post_type'], $dp['post_status']);
@@ -75,7 +75,7 @@
 	message('Posts Updated');
 
 	//Add relationship for post and tags
-	$drupal_post_tags = $dc->results("SELECT DISTINCT node.nid, taxonomy_term_data.tid FROM (".$DB_DRUPAL_PREFIX."taxonomy_index taxonomy_index INNER JOIN ".$DB_DRUPAL_PREFIX."taxonomy_term_data taxonomy_term_data ON (taxonomy_index.tid = taxonomy_term_data.tid)) INNER JOIN ".$DB_DRUPAL_PREFIX."node node ON (node.nid = taxonomy_index.nid)"); 
+	$drupal_post_tags = $dc->results("SELECT DISTINCT node.nid, term_data.tid FROM (".$DB_DRUPAL_PREFIX."term_node term_node INNER JOIN ".$DB_DRUPAL_PREFIX."term_data term_data ON (term_node.tid = term_data.tid)) INNER JOIN ".$DB_DRUPAL_PREFIX."node node ON (node.nid = term_node.nid)"); 
 	foreach($drupal_post_tags as $dpt)
 	{
 		$wordpress_term_tax = $wc->row("SELECT DISTINCT term_taxonomy.term_taxonomy_id FROM ".$DB_WORDPRESS_PREFIX."term_taxonomy term_taxonomy  WHERE (term_taxonomy.term_id = ".$dpt['tid'].")"); 
@@ -92,10 +92,10 @@
 	message('Tags Count Updated');
 
 	//Get the url alias from drupal and use it for the Post Slug
-	$drupal_url = $dc->results("SELECT url_alias.source, url_alias.alias FROM ".$DB_DRUPAL_PREFIX."url_alias url_alias WHERE (url_alias.source LIKE 'node%')");
+	$drupal_url = $dc->results("SELECT url_alias.src, url_alias.dst FROM ".$DB_DRUPAL_PREFIX."url_alias url_alias WHERE (url_alias.src LIKE 'node%')");
 	foreach($drupal_url as $du)
 	{
-		$update = $wc->query("UPDATE ".$DB_WORDPRESS_PREFIX."posts SET post_name = '%s' WHERE ID = '%s'",$du['alias'],str_replace('node/','',$du['source']));
+		$update = $wc->query("UPDATE ".$DB_WORDPRESS_PREFIX."posts SET post_name = '%s' WHERE ID = '%s'",$du['dst'],str_replace('node/','',$du['src']));
 	}
 	message('URL Alias to Slug Updated');
 
